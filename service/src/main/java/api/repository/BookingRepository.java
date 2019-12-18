f46a7f31250fed6734c87d823d5c212c8797c624
mscbee <charlenebadibanga@gmail.com>
2019-12-18 17:49:55 +0000

    imported CrudRepository class for implementation of main CRUD operations

--- service/src/main/java/api/repository/BookingRepository.java
+++ service/src/main/java/api/repository/BookingRepository.java
@@ -2,23 +2,12 @@ package api.repository;
 
 import api.models.Booking;
 import org.springframework.data.repository.CrudRepository;
-import api.models.Interpreter;
 
-import java.util.List;
-
-public interface BookingDatabase extends CrudRepository<Booking, Integer> {
-
-   // void addBooking(Booking booking);
-   // void editBooking(Booking booking);
-
-   // void acceptBooking(Booking booking, Interpreter interpreter);
+public interface BookingRepository extends CrudRepository<Booking, Integer> {
 
     void delete(Booking booking);
 
     Booking findBookingById(int id);
 
     Iterable<Booking> findAll();
-
-
-
 }
