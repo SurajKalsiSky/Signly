e03d12d9532c9717a900336154d4b77060f94721
Suraj Kalsi <Suraj.Kalsi@sky.uk>
2019-12-20 00:47:22 +0000

    cleanup

--- app/src/components/navbar.js
+++ app/src/components/navbar.js
@@ -2,12 +2,9 @@ import React, { Component } from 'react';
 import './navbar.css';
 
 import Logo from '../assets/logo2.png';
-// import { FaAlignRight } from 'react-icons/fa';
-// import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
     render() {
-        // const { isOpen } = this.state;
         return (
             <nav className="navbar">
                 <div className="nav-center">
@@ -15,11 +12,9 @@ export default class Navbar extends Component {
                         <img src={Logo} alt="Signly" className="nav-logo" />
                     </div>
                     <ul className={"nav-links show-nav"}>
-                        <button type="button" className="nav-btn" onClick={this.handleToggle}>
+                        {/* <button type="button" className="nav-btn" onClick={this.handleToggle}>
                             Press me
-                        </button>
-                        {/* <li><Link to="/">Home</Link></li>
-                        <li><Link to="/rooms">Rooms</Link></li> */}
+                        </button> */}
                     </ul>
                 </div>
             </nav>
