40f34a5ffbdad06acdf7c000950872ad172150f7
Kavita Sharma <kavita.sharma@sky.uk>
2020-01-30 20:00:42 +0000

    Added a form to create new bookings

--- app/src/pages/makebookingpage.js
+++ app/src/pages/makebookingpage.js
@@ -1,34 +1,40 @@
 import React, { Component } from 'react';
 import BackButton from '../components/backbutton';
 import EmployeeTable from '../components/table';
-import { Button } from 'semantic-ui-react'
+import { Button, Form  } from 'semantic-ui-react';
 import './homepage.css';
 const employeeBookingsMock = require('../mocks/employeeBookings.json');
 
 export default class MakeBookingPage extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            tableData: null
-        }
-    }
+    state = {}
 
-    noBookings = () => (<p className="no-bookings"> You have no bookings </p>)
+    handleChange = (e, { value }) => this.setState({ value })
 
     render() {
+        const { value } = this.state
         return (
             <div>
-                <BackButton goBackAPage={this.props.goBackAPage} />
-                <div className="Homepage">
-                    <header className="Homepage-welcome-message Bookings-view">
-                        <p> Create new booking page </p>
-                        <div>
-                            <Button content='View all bookings' basic color='teal' size='small' onClick={() => this.props.goBackAPage()} />
-                            <Button disabled basic color='teal' size='small'>Make new booking</Button>
-                        </div>
-                        
-                        {this.state.tableData ? <EmployeeTable tableData={employeeBookingsMock} /> : this.noBookings()}
-                    </header>
+            <BackButton goBackAPage={this.props.goBackAPage} />
+                <div className="Make-booking-page">
+                    <Form>
+                        <h3>Appointment Details</h3>
+                        <Form.Input fluid label='Booking Title' placeholder='Title' />
+                        <Form.Input fluid label='First Name' placeholder='First Name' />
+                        <Form.Input fluid label='Last Name' placeholder='Last Name' />
+                        <Form.Input type='date' label='Start Date' placeholder='Date' />
+                        <Form.Group inline>
+                            <Form.Input label='Start Time' placeholder='Start Time' />
+                            <Form.Input label='Duration' placeholder='HH:MM' />
+                        </Form.Group>   
+                        <Form.Input fluid label='Company' placeholder='Company' />
+                        <Form.Input fluid label='Email Address' placeholder='Email' />
+                    </Form>
+
+                    <div style={{marginTop: '10px'}}>
+                        <Button floated='right' color='teal' onClick={() => this.props.goBackAPage()}>
+                            Submit
+                        </Button>
+                    </div>
                 </div>
             </div>
         )
